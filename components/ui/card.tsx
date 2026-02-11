afe932b42867d440f9a38509eb8c0f01401646be
gestaosuas <gestaosuas@uberlandia.mg.gov.br>
2026-02-11 12:41:11 -0300

    style: darken background and add elevated shadows to cards

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -8,7 +8,7 @@ const Card = React.forwardRef<
     <div
         ref={ref}
         className={cn(
-            "rounded-lg border bg-card text-card-foreground shadow-sm",
+            "rounded-xl border bg-white text-card-foreground shadow-xl shadow-blue-900/10",
             className
         )}
         {...props}
